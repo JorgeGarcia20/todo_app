25fa96a085c9f70d09cf1d3bb87764e89b77aff9
Jorge Alberto Garcia Estrada <alberto-garcia20@outlook.com>
2022-06-05 10:50:36 -0500

    Creacion y configuracion de variables de entorno

---
+++ config.py
@@ -0,0 +1,13 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+SECRET_KEY = os.environ['SECRET_KEY']
+
+user = os.environ['MYSQL_USER']
+password = os.environ['MYSQL_PASSWORD']
+host = os.environ['MYSQL_HOST']
+database = os.environ['MYSQL_DATABASE']
+
+DATABASE_CONNECTION_URI = f'mysql://{user}:{password}@{host}/{database}'
